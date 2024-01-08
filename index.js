189e685d1fade93b472e0a7da5dfdded2ff74894
Dev_LordK <144548552+DexyScraft@users.noreply.github.com>
2024-01-08 22:27:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var scriptElement = document.createElement('script');
+scriptElement.src = 'script.js';
+document.head.appendChild(scriptElement);
+
+var linkElement = document.createElement('link');
+linkElement.rel = 'stylesheet';
+linkElement.type = 'text/css';
+linkElement.href = 'styles.css';
+linkElement.href = 'scrollbar.css'
+document.head.appendChild(linkElement);
